d1d04bcdc18a82ce2240584e7e2ff0feba6e420c
pradeepxvi <pradeepxvi@gmail.com>
2025-03-15 17:14:48 +0545

    Initial commit template

--- helloproject/views.py
+++ helloproject/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
 
 def Hello(request):
-    return HttpResponse("hello world..")
+    return render(request, 'hello.html')
